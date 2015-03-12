bdf25d4a1b8b1028ac3e81cef5a7026df25d6ab1
therealdsmart <davidsmart21@gmail.com>
2015-03-12 01:01:18 -0400

    chips

--- js/client.js
+++ js/client.js
@@ -19,17 +19,3 @@ user.signUp(null, {
 
 
 
-document.getElementById("in").onclick=   function logMeIn(){
-		  Parse.FacebookUtils.logIn(null, {
-		  success: function(user) {
-		    if (!user.existed()) {
-		      alert("User signed up and logged in through Facebook!");
-		    } else {
-		      alert("User logged in through Facebook!");
-		    }
-		  },
-		  error: function(user, error) {
-		    alert("User cancelled the Facebook login or did not fully authorize.");
-		  }
-		});
-		}
