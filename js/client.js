2a86333cef00b429405144c127372e0a61b9ceab
therealdsmart <davidsmart21@gmail.com>
2015-03-11 23:53:58 -0400

    ...

--- js/client.js
+++ js/client.js
@@ -17,18 +17,3 @@ user.signUp(null, {
     alert("Error: " + error.code + " " + error.message);
   }
 });}
-
-function logIn() {} {
-  Parse.FacebookUtils.logIn(null, {
-  success: function(user) {
-    if (!user.existed()) {
-      alert("User signed up and logged in through Facebook!");
-    } else {
-      alert("User logged in through Facebook!");
-    }
-  },
-  error: function(user, error) {
-    alert("User cancelled the Facebook login or did not fully authorize.");
-  }
-});
-}
