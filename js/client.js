bef0b43dfb174b02943e5a52d35b60c20f671e38
therealdsmart <davidsmart21@gmail.com>
2015-03-11 23:01:19 -0400

    Initialization

---
+++ js/client.js
@@ -0,0 +1,19 @@
+Parse.initialize("m6cPbArnJyhg0yoMeAwU5bpClSXWjZnuTOcaChuq", "3z3mull3Uyyqs6bJLfNTt0uU4sZ5H1VEkjD75NBp");
+
+function newUser() {var user = new Parse.User();
+user.set("username", "therealdsmart");
+user.set("password", "ayylmao123");
+user.set("email", "davidsmart21@gmail.com");
+  
+// other fields can be set just like with Parse.Object
+user.set("phone", "270 303 4449");
+  
+user.signUp(null, {
+  success: function(user) {
+    // Hooray! Let them use the app now.
+  },
+  error: function(user, error) {
+    // Show the error message somewhere and let the user try again.
+    alert("Error: " + error.code + " " + error.message);
+  }
+});}
