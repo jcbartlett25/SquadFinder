5b4da0dc45f08b34cc9f38a91bfb320a810f6bd5
therealdsmart <davidsmart21@gmail.com>
2015-03-14 18:36:14 -0400

    no

--- js/client.js
+++ js/client.js
@@ -1,3 +1,34 @@
+	//signs up a new user
+	function newUser() {
+		var user = new Parse.User();
+		user.set("username", document.getElementById('username').text);
+		user.set("password", document.getElementById('password').text);
+		//user.set("email", document.getElementById('email').text);
+		//user.set("phone", document.getElementById('phone').text);
+		  
+		user.signUp(null, {
+		  success: function(user) {
+		    // Hooray! Let them use the app now.
+		    alert("You have been signed up! Squad UP!");
+		  },
+		  error: function(user, error) {
+		    // Show the error message somewhere and let the user try again.
+		    alert("Error: " + error.code + " " + error.message);
+		  }
+		});}
+
+	function logIn(){
+
+	  Parse.User.logIn(document.getElementById('username').text, document.getElementById('password').text), {
+	  success: function(user) {
+	    // Do stuff after successful login.
+	    location.href = "Profile.html";
+	  },
+	  error: function(user, error) {
+	    // The login failed. Check error to see why.
+	  }
+	};
+	}
 
 
 
