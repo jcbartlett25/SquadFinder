3ff9644917184caf3c20b78fa09c162740e398ff
therealdsmart <davidsmart21@gmail.com>
2015-03-14 17:07:55 -0400

    **** Code is Cleaner! *****
    I have commented all the code in the html so that it is clear. Try to
    follow my style of commenting. Usually, I write a comment for large
    sections of the page (header, content, footer, etc.), sections of the
    page that may not be as clear, and scripts. Also, be sure to use
    template.html to build new pages

--- js/client.js
+++ js/client.js
@@ -1,21 +1,3 @@
-function newUser() {
-var user = new Parse.User();
-user.set("username", "therealdsmart");
-user.set("password", "ayylmao123");
-user.set("email", "davidsmart21@gmail.com");
-  
-// other fields can be set just like with Parse.Object
-user.set("phone", "270 303 4449");
-  
-user.signUp(null, {
-  success: function(user) {
-    // Hooray! Let them use the app now.
-  },
-  error: function(user, error) {
-    // Show the error message somewhere and let the user try again.
-    alert("Error: " + error.code + " " + error.message);
-  }
-});}
 
 
 
