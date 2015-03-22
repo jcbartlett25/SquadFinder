2e4334e476c29f92124ce330fac673b4f05abfc3
therealdsmart <davidsmart21@gmail.com>
2015-03-21 21:08:55 -0400

    maybe

--- js/on_start.js
+++ js/on_start.js
@@ -1,3 +1,28 @@
+      // Initialize Parse
+      Parse.initialize("m6cPbArnJyhg0yoMeAwU5bpClSXWjZnuTOcaChuq", "3z3mull3Uyyqs6bJLfNTt0uU4sZ5H1VEkjD75NBp");
+     
+     //Facebook 
+      window.fbAsyncInit = function() {
+        Parse.FacebookUtils.init({ // this line replaces FB.init({
+          appId      : '697679580354991', // Facebook App ID
+          status     : false,  // check Facebook Login status
+          cookie     : true,  // enable cookies to allow Parse to access the session
+          xfbml      : true,  // initialize Facebook social plugins on the page
+          version    : 'v2.2' // point to the latest Facebook Graph API version
+        });
+     
+        // Run code after the Facebook SDK is loaded.
+    };
+      
+      //Loads Facebook SDK
+      (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+
 var profpic = FB.api(
     "/me/picture",
     {
