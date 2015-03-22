d81546697488b6ee28697a288828f23c9b16ed1b
therealdsmart <davidsmart21@gmail.com>
2015-03-22 02:16:31 -0400

    trying to fix login

--- js/client.js
+++ js/client.js
@@ -23,7 +23,8 @@
 	  Parse.User.logIn(document.getElementById('username').value, document.getElementById('password').value), {
 	  success: function(user) {
 	    // Do stuff after successful login.
-	    window.open('feed.html', "_self");
+	    alert('YEET');
+	    window.open("feed.html", "_self");
 	  },
 	  error: function(user, error) {
 	    // The login failed. Check error to see why.
