c36e48bd27ce0fc52e30de432612ac29a24095b2
jcbartlett25 <jcbart12595@gmail.com>
2015-03-22 12:58:51 -0400

    Erasing newUser(), creating new function from scratch

--- js/client.js
+++ js/client.js
@@ -1,6 +1,3 @@
-src="http://cdn.jsdelivr.net/jquery/2.1.1/jquery.min.js"
-src="http://cdn.jsdelivr.net/syncanojs/3.1.3beta/syncano.min.js"
-src="http://cdn.jsdelivr.net/sockjs/0.3.4/sockjs.min.js"
 	//signs up a new user
 	function newUser() {
 		var user = new Parse.User();
@@ -15,39 +12,6 @@ src="http://cdn.jsdelivr.net/sockjs/0.3.4/sockjs.min.js"
 		user.signUp(null, {
 		  success: function(user) {
 		    // Hooray! Let them use the app now.
-		    var syncano = SyncanoConnector.getInstance(); 
-		    var PROJECT_ID = 6289;
-			var COLLECTION_ID = 18895;
-
-				//Keys
-  			var authData = {
-    			api_key: "b50a00e33bb198286b779a53666249b90eb3f6dc",
-    			instance: "sparkling-meadow-922472"
-  			};
-  
-  			syncano.connect(authData, function (auth) {
-    			console.log("Connected");
-    			});
-  			syncano.on('syncano:authorized', function(auth){
-    			console.log("authorized");
-  				});
-  			
-    			//Stores User in an object
-    			var params = {
-    				title: username,
-    				text: 'a valued user',
-    				state: 'Moderated',
-   					additional: {
-   						squads: [],
-   						squadrons: []
-   					}
-  				};
-  				
-  				//Actually pushes object to database
-  				syncano.Data.new(PROJECT_ID, COLLECTION_ID, params, function(data){
-    				console.log('Created new data object with ID = ', data.id);
-  					});
-
 		    window.open('feed.html', "_self");
 		  },
 
@@ -55,7 +19,7 @@ src="http://cdn.jsdelivr.net/sockjs/0.3.4/sockjs.min.js"
 		    // Show the error message somewhere and let the user try again.
 		    alert("Error: " + error.code + " " + error.message);
 		  }
-		})}
+		})};
 
 	function login(){
 	  Parse.User.logIn(document.getElementById('username').value, document.getElementById('password').value, {
