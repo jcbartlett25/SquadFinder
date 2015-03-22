f607d38dbbb42b14f83261556edddc620230d2ed
jcbartlett25 <jcbart12595@gmail.com>
2015-03-22 05:07:38 -0400

    added parentheses

--- js/client.js
+++ js/client.js
@@ -23,11 +23,10 @@
 	  success: function(user) {
 	    // Do stuff after successful login.
 	   		window.open("feed.html", "_self");
-	   		location.reload();
 	  	},
 	  		error: function(user, error) {
 	    // The login failed. Check error to see why.
-	  	}
+	  	})
 
 		}
 
