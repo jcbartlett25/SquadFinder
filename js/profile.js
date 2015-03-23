a7cbebd06ac62488e0ae5e50b6a3d65cb2355318
therealdsmart <davidsmart21@gmail.com>
2015-03-23 19:29:15 -0400

    more profile pic fixes

--- js/profile.js
+++ js/profile.js
@@ -1,8 +1,24 @@
 function upload_pic(){
-	var pic = document.getElementById("pic_upload");
-	var profile_pic = new Parse.File("photo.jpg", pic);
-	$("#profile_pic").html(profile_pic);
 	var user = Parse.User.current();
 
-	user.set('profile_pic', profile_pic);
+	var fileUploadControl = $("#pic_upload")[0];
+	if (fileUploadControl.files.length > 0) {
+  	var file = fileUploadControl.files[0];
+
+  	var name = "photo.jpg";
+  	var parseFile = new Parse.File(name, file);
+
+  	parseFile.save().then(function() {
+  // The file has been saved to Parse.
+	}, function(error) {
+  // The file either could not be read, or could not be saved to Parse.
+	});
+
+  	$("#profile_pic").attr('src', parseFile)
+
+  	user.set('profile_pic', parseFile);
+
+  	var profilePhoto = user.get("profile_pic");
+	$("profile_pic")[0].src = profilePhoto.url();
+	
 }
