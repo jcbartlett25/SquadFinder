fc5ff93a2f464f211b665b128ec6e390bb3bb293
therealdsmart <davidsmart21@gmail.com>
2015-03-23 19:44:29 -0400

    again

--- js/profile.js
+++ js/profile.js
@@ -1,22 +1,26 @@
 function upload_pic(){
+	// gets user
 	var user = Parse.User.current();
 
+	//reference uploaded file
 	var fileUploadControl = $("#pic_upload")[0];
 	if (fileUploadControl.files.length > 0) {
   	var file = fileUploadControl.files[0];
-
   	var name = "photo.jpg";
   	var parseFile = new Parse.File(name, file);
 
+  	//Save uploaded file
   	parseFile.save().then(function() {
   // The file has been saved to Parse.
 	}, function(error) {
   // The file either could not be read, or could not be saved to Parse.
 	});
 
-  	$("#profile_pic").attr('src', parseFile)
+  	//$("#profile_pic").attr('src', parseFile)
 
+  	//set user's profile pic to uploaded file
   	user.set('profile_pic', parseFile);
+
 
   	var profilePhoto = user.get("profile_pic");
 	$("#profile_pic")[0].src = profilePhoto.url();
