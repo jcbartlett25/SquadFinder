ba9b853b6c2ec8c3d4443268d5f1afeae5be1f1d
jcbartlett25 <jcbart12595@gmail.com>
2015-03-23 00:45:56 -0400

    adding squads and squadrons to each user objects
    self explanatory

--- js/client.js
+++ js/client.js
@@ -15,6 +15,8 @@
 		user.set("username", username);
 		user.set("password", password);
 		user.set("email", email);
+		user.set("squads", []);
+		user.set("squadrons", []);
 		//user.set("phone", document.getElementById('phone').text);
 		  
 		user.signUp(null, {
