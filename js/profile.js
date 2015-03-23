13105f011828462b5eaea9e17512a768123d8c34
therealdsmart <davidsmart21@gmail.com>
2015-03-23 18:24:16 -0400

    2nd attempt

--- js/profile.js
+++ js/profile.js
@@ -2,4 +2,7 @@ function upload_pic(){
 	var pic = document.getElementById("pic_upload");
 	var profile_pic = new Parse.File("photo.jpg", pic);
 	$("#profile_pic").html(profile_pic);
+	var user = Parse.User.current();
+
+	user.set('profile_pic', profile_pic);
 }
