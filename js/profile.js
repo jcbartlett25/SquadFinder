d6e57a21f3618498c22f337a42e40bbe0dc0a8e3
therealdsmart <davidsmart21@gmail.com>
2015-03-23 20:12:17 -0400

    directly input link

--- js/profile.js
+++ js/profile.js
@@ -32,5 +32,5 @@ function load_pic(){
 	var user = Parse.User.current();
 	var profilePhoto = user.get("profile_pic");
 	var profileURL = profilePhoto.URL();
-	$("#profile_pic").attr('src', profileURL);
+	$("#profile_pic").attr('src', "http://files.parsetfss.com/3be3fd11-410d-4010-99a3-1304e98bdd9a/tfss-9bdc8c5a-e682-4253-9033-d9515e202c5d-Snapchat-20140724052553_opt.jpg");
 }
