af02bad319ce63ec4f25e9f7056cdee38ba0494a
jcbartlett25 <jcbart12595@gmail.com>
2015-03-27 23:26:11 -0400

    Reorganizing feed.js

--- js/feed.js
+++ js/feed.js
@@ -9,53 +9,71 @@ $(document).ready(
   );
 
 //POPULATES Feed with Posts
-//Connects to Syncano
-var syncano = SyncanoConnector.getInstance(); 
-var authData = {
-  api_key: "b50a00e33bb198286b779a53666249b90eb3f6dc",
-  instance: "sparkling-meadow-922472"
-};
-var PROJECT_ID = 6289;
-var COLLECTION_ID = 18888;
-  
-syncano.connect(authData, function (auth) {
-  console.log("Connected");
-});
-  
-syncano.on('syncano:authorized', function(auth){
-  console.log("authorized");
-});
-  
-var params = {
-  include_children: false,
-  folders: 'Default'
-}
 
-// Pulls post objects from Syncano and fills in each template
-syncano.Data.get(PROJECT_ID, COLLECTION_ID, params, function (data) {
-  console.log('Received', data.length, 'objects');
-  //looping through each object received from Syncano and filling the template
-  for (i in data) {
-   	var obj = data[i];
-  	$("#squad_descript").html(obj.text);
-		$("#squad_title").html(obj.title);
-		$("#post_username").html(obj.additional.username);
-		$post = $("#template").clone();
-    //Gives each div a unique name
-    $post.attr("id", obj.id);
-		$(".feed_div").prepend($post);
-		$post.fadeIn();
+function populatePage(){
+  //Connects to Syncano
+  var syncano = SyncanoConnector.getInstance(); 
+  var authData = {
+    api_key: "b50a00e33bb198286b779a53666249b90eb3f6dc",
+    instance: "sparkling-meadow-922472"
   };
-  
-  //Logging each object to the console			
-  data.forEach(function (d) {
-   	console.log(d);
+  var PROJECT_ID = 6289;
+  var COLLECTION_ID = 18888;
+    
+  syncano.connect(authData, function (auth) {
+    console.log("Connected");
   });
-});
+    
+  syncano.on('syncano:authorized', function(auth){
+    console.log("authorized");
+  });
+    
+  var params = {
+    include_children: false,
+    folders: 'Default'
+  }
 
-syncano.off();
+  // Pulls post objects from Syncano and fills in each template
+  syncano.Data.get(PROJECT_ID, COLLECTION_ID, params, function (data) {
+    console.log('Received', data.length, 'objects');
+    //looping through each object received from Syncano and filling the template
+    for (i in data) {
+     	var obj = data[i];
+    	new squadPost(obj.text, obj.title, obj.additional.username, obj.id);
+    };
+    //Logging each object to the console			
+    data.forEach(function (d) {
+     	console.log(d);
+    });
+  });
+
+  syncano.off();
+};
 
 
+function squadPost(descript, title, username, id){
+
+  this.descript = descript,
+  this.title = title,
+  this.username = username,
+  this.id = id
+
+  $("#squad_descript").html(descript);
+  $("#squad_title").html(title);
+  $("#post_username").html(username);
+  $post = $("#template").clone();
+  //Gives each div a unique name
+  $post.attr("id", id);
+  $(".feed_div").prependTo($post);
+  $post.fadeIn();
+
+  $post.click(function()
+  {
+    alert(username);
+  })
+
+};
+populatePage();
 //POST A SQUAD
 function postSquad(){
   var descript = document.getElementById("new_post_descript").value;
@@ -79,7 +97,8 @@ function postSquad(){
   $("#squad_title").html(title);
   $("#post_username").html(username);
   $post = $("#template").clone();
-  $post.removeAttr('id');
+  //Gives each div a unique name
+  $post.attr("id", obj.id);
   $(".feed_div").prepend($post);
   $post.fadeIn();
 
