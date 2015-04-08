584216d788d696177862be9ef7b336e27aa559e7
jcbartlett25 <jcbart12595@gmail.com>
2015-04-08 16:43:32 -0400

    Ready for time?

--- js/feed.js
+++ js/feed.js
@@ -192,32 +192,47 @@ function encodeHTML(s) {
   return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
 }
 
-function timeSince(date) {
+var timeSince = function(date) {
+    if (typeof date !== 'object') {
+        date = new Date(date);
+    }
 
-    var seconds = Math.floor((new Date().getTime() - date) / 1000);
+    var seconds = Math.floor((new Date() - date) / 1000);
+    var intervalType;
 
     var interval = Math.floor(seconds / 31536000);
+    if (interval >= 1) {
+        intervalType = 'year';
+    } else {
+        interval = Math.floor(seconds / 2592000);
+        if (interval >= 1) {
+            intervalType = 'month';
+        } else {
+            interval = Math.floor(seconds / 86400);
+            if (interval >= 1) {
+                intervalType = 'day';
+            } else {
+                interval = Math.floor(seconds / 3600);
+                if (interval >= 1) {
+                    intervalType = "hour";
+                } else {
+                    interval = Math.floor(seconds / 60);
+                    if (interval >= 1) {
+                        intervalType = "minute";
+                    } else {
+                        interval = seconds;
+                        intervalType = "second";
+                    }
+                }
+            }
+        }
+    }
 
-    if (interval >= 1) {
-        return interval + " years ago";
+    if (interval > 1 || interval === 0) {
+        intervalType += 's';
     }
-    interval = Math.floor(seconds / 2592000);
-    if (interval >= 1) {
-        return interval + " months ago";
-    }
-    interval = Math.floor(seconds / 86400);
-    if (interval >= 1) {
-        return interval + " days ago";
-    }
-    interval = Math.floor(seconds / 3600);
-    if (interval >= 1) {
-        return interval + " hours ago";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval >= 1) {
-        return interval + " minutes ago";
-    }
-    return Math.floor(seconds) + " seconds ago";
-}
+
+    return interval + ' ' + intervalType;
+};
 
 populatePage();
