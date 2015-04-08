1524e39148f13771c3c2a4bdbf2fefaa5570a3f7
jcbartlett25 <jcbart12595@gmail.com>
2015-04-08 16:55:47 -0400

    Making time look pretty again. Says "1 day ago" instead of 24 hours

--- js/feed.js
+++ js/feed.js
@@ -232,7 +232,7 @@ var timeSince = function(date) {
         intervalType += 's';
     }
 
-    return interval + ' ' + intervalType;
+    return interval + ' ' + intervalType + ' ago';
 };
 
 populatePage();
